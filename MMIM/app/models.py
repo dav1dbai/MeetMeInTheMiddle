e71fb2cc924e4b2af24a2077be7d2de3aeb49b82
Vivien Chen <vivienchen2005@gmail.com>
2023-11-12 14:06:26 -0800

    Revert "Merge branch 'viv-)' of https://github.com/dav1dbai/MeetMeInTheMiddle into viv-)"
    This reverts commit a0cb719f03b80bf77eccac84d488bdad2f047e9f, reversing
    changes made to a03f9c4a01089161cb2f64c52d9706336d6a23c4.

--- MMIM/app/models.py
+++ MMIM/app/models.py
@@ -1,6 +1,5 @@
 import json
 import cohere
-from geopy.geocoders import Nominatim
 
 co = cohere.Client('nTI4k8KctOt7AiB6JdKIJhnwDUeU1BzauNM6G6Op')
 
@@ -34,15 +33,15 @@ class Friend(models.Model):
     def getRec(self):
         location = self.location
         ulocation = self.getLocation()
-        # print(ulocation)
-        # print(location)
+        print(ulocation)
+        print(location)
         prompt = '''
             Find a location equidistant between {} and {}. 
             List some attractions and activities there along with a brief description.
             Include sources. 
             '''
         prompt = prompt.format(ulocation, location)
-        # print(prompt)
+        print(prompt)
         streaming_gens = getResponse(prompt)
 
         json_obj = {
@@ -54,9 +53,3 @@ class Friend(models.Model):
     def get_all_objects(self):
         queryset = self._meta.model.objects.all()
         return queryset
-
-    def getLatLong(self):
-        geolocator = Nominatim(user_agent="davidbai2020@gmail.com")
-        location = geolocator.geocode(self.location)
-        out = [location.longitude, location.latitude]
-        return out
